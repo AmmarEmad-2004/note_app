97967a157b1dbdead88ea0abe773fff1452c36f8
AmmarEmad-2004 <za0662003@email.com>
2025-01-22 16:21:13 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const NotesApp(), // Wrap your app
+    ),
+  );
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
