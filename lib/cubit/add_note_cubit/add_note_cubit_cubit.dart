0d576f4a84caf6d2403943517cb1450a26c9be2c
AmmarEmad-2004 <za0662003@email.com>
2025-01-25 14:43:26 +0200

    implement add note functionality with cubit state management; update dependencies and register note model adapter

---
+++ lib/cubit/add_note_cubit/add_note_cubit_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:meta/meta.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/models/note_model.dart';
+
+part 'add_note_cubit_state.dart';
+
+class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
+  AddNoteCubitCubit() : super(AddNoteCubitInitial());
+  addNote(NoteModel note) {
+    emit(AddNoteCubitLoading());
+    try {
+      var notesBox = Hive.box<NoteModel>(kNotesBox);
+      notesBox.add(note);
+      emit(AddNoteCubitSuccess());
+    } catch (e) {
+      emit(
+        AddNoteCubitFailure(errMassage: e.toString()),
+      );
+    }
+  }
+}
